49359e05b4aaf0184428fba9c1779e86f770f170
amenabar88 <joaquinjararamirez05@gmai.com>
2023-12-13 15:26:50 -0300

    fase 0

---
+++ practica.js
@@ -0,0 +1,24 @@
+/*let numero1 = prompt ("Ingrese el primer numero;");
+let numero2 = prompt ("Ingrese el segundo numero;");
+
+numero1 = Number(1)
+numero2 = Number(2)
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2
+
+console.log("suma", suma);
+console.log("resta", resta);
+console.log("multiplicacion", multiplicacion);
+console.log("division", division);
+
+alert("Resultados:\nSuma: " + suma + "\nResta: " + resta + "\nMultiplicación: " + multiplicacion + "\nDivisión: " + division);*/
+
+/*let nombre = prompt ("hola, ingrese su nombre");
+let apellido = prompt ("por favor, ingrese su apellido")
+
+if (nombre = "joaquin" || "Joaquin" && apellido = "jara" || "Jara"){
+(console.log("bienvenido, Joaquin"))}
+else { (console.log("No eres Joaquin, no puedes ingresar."))}*/
